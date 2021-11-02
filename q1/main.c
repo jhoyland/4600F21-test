70824adc275fcdd7f6778157f47ad3d6715857ef
dcollie <dallas.collie@email.kpu.ca>
2021-11-02 12:00:01 -0700

    test finished

--- q1/main.c
+++ q1/main.c
@@ -8,10 +8,66 @@ write a main function to use your function to find the roots of several polynomi
 you may have your function in the main.c file or create and link a separate file as you wish
 
 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
+void findRoots(double A, double B, double C, double *x)
+{
+
+  if (A==0)
+  {
+    x[0] = -C/B;
+  }
+  else
+  {
+    double firstPart = B*B - 4*A*C;
+    if (firstPart > 0)
+    {
+      x[0] = (-B + sqrt(firstPart))/(2*A);
+      x[1] = (-B - sqrt(firstPart))/(2*A);
+    }
+    else
+    {
+      x[0] = -B /(2*A);
+      x[1] = sqrt(-firstPart);
+      x[2] = 1;
+    }
+
+  }
+
+}
 
 int main(int argc, char** argv)
 {
+  double A[5] = {-4,-4,-8,-6,-56};
+  double B[5] = {-5,-5,-34,25,2};
+  double C[5] = {-9,-9,8,9,10};
+
+
+  for(int i = 0; i < 5;i++ )
+  {
+    double x[3]={0,0,0};
+    findRoots(A[i],B[i],C[i],x);
+
+    if (x[1]==0)
+    {
+      printf("x = %g \n", x[0]);
+    }
+    else if (x[2]==0)
+    {
+      printf("x = %g and %g \n", x[0], x[1]);
+    }
+    else
+    {
+    printf("x = %g + %gi and %g - %gi \n", x[0], x[1], x[0], x[1]);
+    }
+  }
+
+
+
+
+
 
 
   return 0;
--- q2/main.c
+++ q2/main.c
@@ -4,10 +4,66 @@ QUESTION 2:
 
 	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+double factorial(int num)								//function to calculate the factorial
+{
+	double result = 1;
+
+	if (num !=0)
+	{
+		for (int i=1;i <= num; i++)
+
+		{
+			result = result * i;
+		}
+	}
+	return result;
+
+}
+
+double cosExpansion(double x, int terms)
+{
+	double result = 0;
+	for (int i=1; i < terms; i+2)
+	{
+		result = result + pow(-1,i-1)*pow(x,i-1)/factorial(i-1);
+	}
+
+	return result;
+}
+
+double sinExpansion(double x, int terms)
+{
+	double result = x;
+	for (int i=1; i < terms; i+2)
+	{
+		result = result + pow(-1,i)*pow(x,i)/factorial(i);
+	}
+
+	return result;
+}
 
 
 int main(int argc, char** argv)
 {
+	int terms = 0;
+	double x = 1/2;
+	
+	double cosResult = 0;
+	double sinResult = 0;
+
+	printf("Cos and Sin series expansion, how many terms would you like? ");
+	fflush(stdout);
+	scanf("%d",&terms);			
+	
+	cosResult = cosExpansion(x,terms);    //for some reason the program keeps getting stuck here an will not move on
+	sinResult = sinExpansion(x,terms);
+	
+	printf("\nCos espansion is = %g", cosResult);
+	printf("\nSin expansion is = %g", sinResult);
 
 
   return 0;
