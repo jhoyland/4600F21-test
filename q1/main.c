e40aee5059652d2a9d2a3ae769f7b7dd556f2778
Erinn <erinn.psajd@gmail.com>
2021-11-02 11:55:09 -0700

    Test Finished

--- q1/main.c
+++ q1/main.c
@@ -9,10 +9,89 @@ you may have your function in the main.c file or create and link a separate file
 
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+double Root(double* X, double A, double B, double C)
+{
+	double x1, x2, x;
+	double imgcheck;
+	double img1, img2;
+	//solve for simplest case first if possible
+	if(A == 0)
+	{	
+		x = -(C/B);
+		printf("\nThe root of the quadratic is %5.2f \n",x);
+		printf("There is no secondary root.\n");
+		X[0] = x;
+		return 0;
+	}
+	else
+	{
+		//check for imaginary by way of square root, if negative then complex solution
+		imgcheck = pow(B,2) - (4*A*C);
+		//case for non-imaginary solutions
+		if(imgcheck >= 0)
+		{
+			x1 = (-B + sqrt(imgcheck))/(2*A);
+			x2 = (-B - sqrt(imgcheck))/(2*A);
+			printf("\nThe roots of the quadratic equation are real and are %5.2f and %5.2f \n",x1,x2);
+			X[0] = x1;
+			X[1] = x2;
+			return 0;
+		}
+		//the case for imaginary solutions
+		else
+		{
+			x1 = (-B /(2*A));
+			x2 = (-B /(2*A));
+			img1 = (sqrt(-1*imgcheck)/(2*A));
+			img2 = (sqrt(-1*imgcheck)/(2*A));
+			printf("\nThe roots of this quadratic equation are imaginary and are: \n %5.2f + i%5.2f and %5.2f - i%5.2f \n",x1,img1,x2,img2);
+			img2 = -(sqrt(-1*imgcheck)/(2*A));
+			X[0] = x1;
+			X[1] = img1;
+			X[2] = x2;
+			X[3] = img2;
+
+			return 0;
+		}
+	}
+
+}
 
 int main(int argc, char** argv)
 {
+	//test the function 
+	//first, test simplest case: 
+	double A = 0.0;
+	double B = 2.0;
+	double C = 4.0;
+	printf("The quadratic equation is %5.2fx^2 + %5.2fx + %5.2f",A,B,C);
+	double X[20];
+	// the function already does the printing for me. 
+	Root(X,A,B,C);
+	printf("The function returns %5.2f \n\n",X[0]);
 
 
-  return 0;
+	//then, real roots
+	A = 8.0;
+	B = 20.0;
+	C = 3.0;
+	double X1,X2;
+	printf("The quadratic equation is %5.2fx^2 + %5.2fx + %5.2f",A,B,C);
+	Root(X,A,B,C);
+	printf("The function returns %5.2f, %5.2f \n\n",X[0],X[1]);
+
+	//then, imaginary roots
+	A = 5.0;
+	B = 6.0;
+	C = 7.0;
+	double X3,X4,X5,X6;
+	printf("The quadratic equation is %5.2fx^2 + %5.2fx + %5.2f",A,B,C);
+	Root(X,A,B,C);
+	printf("The function returns %5.2f, %5.2f, %5.2f, %5.2f \n\n",X[0],X[1],X[2],X[3]);
+	fflush(stdout);
+	return 0;
 }
--- q2/main.c
+++ q2/main.c
@@ -5,10 +5,101 @@ QUESTION 2:
 	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
 */
 
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+float Expansion(int type, double x, int n)
+{	
+	int i,j, alt, f;
+	double sin, cos;
+	//type 1 = sin, type 2 = cos
+	//if type is sin then do series expansion with sin
+	sin = x;
+	//printf("%f\n\n", sin);
+	cos = 1;
+	alt = 0;
+	if (type == 1)
+	{	
+		for(i=1; i < n; i++)
+		{
+			f = i;
+			//calculate factorial
+			for (j=(i-1); j > 1; j--)
+			{
+				f *= j;
+			}
+			printf("%d,%d \n\n", i,f);
+			//alternate calls 
+			 if (alt == 0)
+			 {
+			 	sin += -(pow(x,i)/f);
+			 	printf("%f\n\n",sin);
+			 	alt = 1;
+			 	//printf("Altcheck = %d \n\n",alt);
+			 }
+			 else
+			 {
+			 	sin += (pow(x,i)/f);
+			 	alt = 0;
+			 	//printf("Altcheck = %d \n\n",alt);
+			 }
+			
+		}
+
+		return sin;
+	}
+	else
+	{	
+		for(i=1; i < n; i++)
+		{
+			//calculate factorial
+			f = i;
+			for (j=(i-1); j > 1; j--)
+			{
+				f *= j;
+			}
+			//alternate calls 
+			 if (alt == 0)
+			 {
+			 	cos += -(pow(x,i)/f);
+			 	alt = 1;
+			 	//printf("Altcheck = %d \n\n",alt);
+			 }
+			 else
+			 {
+			 	cos += (pow(x,i)/f);
+			 	alt = 0;
+			 	//printf("Altcheck = %d \n\n",alt);
+			 }
+		}
+
+	return cos;
+}
+}
 
 int main(int argc, char** argv)
 {
+	//check sin function
+	float X;
+	char type = 1;
+	double x = 4;
+	int n1 = 8;
+	X = Expansion(type, x, n1);
+	printf("Sin(%5.2f) by expansion of %i factors is %5.6f\n\n",x,n1,X);
 
+	//check cos function
+	float Y;
+	char ty = 2;
+	double y = 3.14;
+	int n2 = 8;
+	Y = Expansion(ty, y, n2);
+	printf("Cos(%5.2f) by expansion of %d factors is %5.6f\n\n",y,n2,Y);
+
+	// both generate incorrect values ): 
+	// checked that alternating variable is working
+	// when I set to 20, alternating seems to be breaking off? 
+	// checked that factorial value value is correct (checked up to 5!)
 
   return 0;
 }
