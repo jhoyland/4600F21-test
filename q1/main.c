4ddfb0c45ac54ef80c92042b3faa6bb6ad5479ee
MichaelYakovlev <yakovlev.michael@icloud.com>
2021-11-02 10:43:30 -0700

    Test finished

--- q1/main.c
+++ q1/main.c
@@ -9,10 +9,70 @@ you may have your function in the main.c file or create and link a separate file
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+// Function that
+void roots_func(float a, float b, float c, float * roots){
+	
+	//Case when coefficients make a linear function
+	if (a==0) {
+		roots[0]=-c/b;
+	}
+
+	//Case when coefficients make a quadratic function
+	else{
+		float b_=-b; //-b term
+		float sqrt_=b*b-4.0*a*c; //value under the square root
+		float denom=2.0*a;// denominator
+
+		//case when value under square root is negative => imaginary roots
+		if (sqrt_<0){
+			roots[1]=sqrt(-sqrt_)/denom;//coefficient for imaginary part
+			roots[0]=b_/denom;//real part
+			roots[2]=2.0; //indicator of the case
+
+		}
+		else{//case when thee quadratic roots are real
+			roots[0]=(b_+sqrt(sqrt_))/denom; //root #1
+			roots[1]=(b_-sqrt(sqrt_))/denom; //root #2
+			roots[2]=1.0; //indicator of the case
+		}
+	}
+}
 
 int main(int argc, char** argv)
 {
+	//Declaration of coefficients for the quadratic
+	float a=5.0;
+	float b=6.0;
+	float c=5.0;
 
+	//Array that will store the type of answer and appropriate roots
+	float roots[3];
 
-  return 0;
+	//Calling function to perform necessary calculations 
+	roots_func(a,b,c,roots);
+
+	//Print of answer for linear equation
+	if (roots[2]==0){
+		printf ("root ofthe equation     %fx + %f = 0     is: ",b,c);
+		printf ("\n\t%f",roots[0]);
+	}
+
+	//Print of answer for quadratic equation with real roots;
+	if (roots[2]==1.0){
+		printf ("root ofthe equation     %fx^2 + %fx + %f =0     are: ",a,b,c);
+		printf ("\n\t%f",roots[0]);
+		printf ("\n\t%f",roots[1]);
+	}
+
+	//Print of answer for quadratic equation with imaginary roots;
+	if (roots[2]==2.0){
+		printf ("root ofthe equation     %fx^2 + %fx + %f = 0     are: ",a,b,c);
+		printf ("\n\t%f + %f(i)",roots[0],roots[1]);
+		printf ("\n\t%f - %f(i)",roots[0],roots[1]);
+	}
+	return 0;
 }
--- q2/main.c
+++ q2/main.c
@@ -5,10 +5,74 @@ QUESTION 2:
 	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//calcualtes factorial of a given number
+double fact(int i){
+	double j=2.0; // smallest value that can be given to this function is 2 (1st term in cos expansion)
+	double value=1.0;
+	while (j<=i){ //multiplies (2)*(3)*(4)*...*(i-2)*(i-1)*(i)
+		value=value*j;
+		j=j+1;
+	}
+	return(value);
+}
+
+//calculates individual terms of the expansion starting with 1st (0th already known) 
+double term(float x, int i){
+	double numer=pow(x,i); //raises x to power
+	double denom=fact(i); // calss and stores result of factorial function
+	return (numer/denom);
+}
+
+//calculates the values of the entire sine expansion
+double sin_expansion(double x, int n){
+	int i=1; //the values of 0th terms need-not to be calculated w/ expressions
+	double answer=x; // expansion always begin with x
+	while (i<n){
+		if (i%2==1){
+			answer=answer-term(x,i*2+1); // every odd term is negative (starting at 0th)
+		}
+		else{
+			answer=answer+term(x,i*2+1); // every even term is positive (starting at 0th)
+		}
+		i=i+1;
+	}
+	return answer;
+}
+
+//calculates the values of the entire cosine expansion
+double cos_expansion(double x, int n){
+	int i=1; //the values of 0th terms need-not to be calculated w/ expressions
+	double answer=1; // expansion always begin with 1
+	while (i<n){
+		if (i%2==1){
+			answer=answer-term(x,i*2); // every odd term is negative (starting at 0th)
+		}
+		else{
+			answer=answer+term(x,i*2); // every even term is positive (starting at 0th)
+		}
+		i=i+1;
+	}
+	return answer;
+}
 
 int main(int argc, char** argv)
 {
 
+	double x=12.0; // value of x to for sin & cos
+	int n=30; //number of terms in the expansion
 
-  return 0;
+	double expans_sin= sin_expansion(x,n); //calling and storing the expansion of sin at n terms for given x
+	float expans_cos= cos_expansion(x,n); //calling and storing the expansion of cos at n terms for given x
+	double true_sin= sin(x); //true value of sin for comparrison
+	double true_cos= cos(x); //true value of cos for comparrison
+
+	printf("\n\n\nexapnsion of sin(%lf)  : %lf",x,expans_sin);
+	printf("\ntrue value of sin(%lf) : %lf\n",x,true_sin);
+	printf("\nexapnsion of cos(%lf)  : %lf",x,expans_cos);
+	printf("\ntrue value of cos(%lf) : %lf\n\n\n",x,true_cos);
+	return 0;
 }
