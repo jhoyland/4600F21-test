463c548e82d3f51c938f7aa140a4ddcce25f88a3
ernie-ng <ernie_ng@outlook.com>
2021-11-02 12:09:08 -0700

    Test finished

--- q1/main.c
+++ q1/main.c
@@ -8,11 +8,66 @@ write a main function to use your function to find the roots of several polynomi
 you may have your function in the main.c file or create and link a separate file as you wish
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void quadratic(double A, double B, double C, double *roots);
 
 
 int main(int argc, char** argv)
 {
+	double A = 1.0;
+	double B = 2.0;
+	double C = 0.0; //x = 0, x = -2
+	double roots[2]; 
+
+	quadratic(A, B, C, roots);
+
+	for(int i = 0; i<2; i++)
+	{
+		printf("x = %g\n", roots[i]);
+	}
 
 
   return 0;
+}
+
+void quadratic(double A, double B, double C, double *roots)
+{
+	//-b+-sqrt(b^2-4ac)/2a
+	//find the determinant first to determine what solutions are there.
+	double determinant = (B*B)-(4*A*C);
+
+	if(determinant > 0) //two real solutions
+	{
+		for(int i=0; i<2;i++)
+		{
+			if(i==0)
+			{
+				roots[i] = (-B+sqrt(determinant))/2.0/A;
+				//printf("got here");
+			}
+			if(i==1)
+			{
+				roots[i] = (-B - sqrt(determinant))/2.0/A;
+				
+			}
+		}
+	}
+	if(determinant == 0) //one real solution (this works)
+	{
+		for(int i = 0; i<2;i++)
+		{
+			roots[i] = -B/2.0/A;
+		}
+	}
+
+	if(determinant < 0) //complex solution, put in i
+	{
+		for(int i = 0; i<2; i++)
+		{
+			roots[i] = -B + sqrt(-(B*B)-(4*A*C))/(2.0*A);
+		}
+	}
+
 }
--- q2/main.c
+++ q2/main.c
@@ -4,11 +4,75 @@ QUESTION 2:
 
 	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+double seriesExpansion(double x, int nterms, int mode);
+double factorial(int x);
 
 
 int main(int argc, char** argv)
 {
-
+	double value = 0.0;
+	value = seriesExpansion(3.14159, 4, 0); //sin(pi) 4 terms
+	//printf("hello\n");
+	printf("%g\n", value);
 
   return 0;
+}
+
+
+double seriesExpansion(double x, int nterms, int mode) //number of terms. mode 0 for sin, 1 for cos
+{
+	double positive = 0.0;
+	double negative = 0.0;
+	double expansion = 0.0;
+	double fact = 0.0;
+	int i=0;
+	int j=0;
+	if(mode == 0)
+	{
+		//when nterms is even, num of positive and negative terms are same.
+		//when nterms is odd,  num of positive is one more than negative.
+		//sinx
+		for(j=1; j<nterms*2; j=j+4) // (x^1), -x^3, (x^5), -x^7
+		{
+			fact = factorial(j);
+			positive += pow(x,j)/fact;
+		}
+		for(i=3; i<=nterms*2; i=i+4) // x^1, (-x^3), x^5, (-x^7)
+		{
+			fact = factorial(i);
+			negative += -pow(x,i)/fact;
+		}
+
+	}
+	if(mode == 1)
+	{
+		//cosx
+		for(j=0; j<nterms*2-3; j=j+4) // (x^0), -x^2, (x^4), -x^6
+		{
+			fact = factorial(j);
+			positive += pow(x,j)/fact;
+		}
+		for(i=2; i<nterms*2; i=i+4) // x^0, (-x^2), x^4, (-x^6)
+		{
+			fact = factorial(i);
+			negative += -pow(x,i)/fact;
+		}
+
+	}
+	expansion = positive + negative;
+	return expansion;
+}
+
+double factorial(int x)
+{
+	double fact = 1.0;
+	int i=0;
+	for(i=1; i<=x; i++)
+	{
+		fact = fact * i;
+	}
+	return fact;
 }
