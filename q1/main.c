7c9323254c540de49b5e951bfeb1bf930ec1f7db
James Hoyland <hoyland.jd@gmail.com>
2021-11-02 07:47:57 -0700

    Test ready

---
+++ q1/main.c
@@ -0,0 +1,18 @@
+/*
+
+QUESTION 1:
+write a function which will return the roots of a quadriatic including complex roots, it should take in the coefficients A,B and C  and should return both roots it should be able to cope with both real and complex roots and if given A = 0 should return the root of the linear equation.
+
+write a main function to use your function to find the roots of several polynomials, printing out the roots including if they are complex numbers.
+
+you may have your function in the main.c file or create and link a separate file as you wish
+
+*/
+
+
+int main(int argc, char** argv)
+{
+
+
+  return 0;
+}
---
+++ q2/main.c
@@ -0,0 +1,14 @@
+/*
+
+QUESTION 2:
+
+	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
+*/
+
+
+int main(int argc, char** argv)
+{
+
+
+  return 0;
+}
