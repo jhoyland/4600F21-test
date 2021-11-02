49cb1e160c2c5e5bda21c4ccb8e09d14c3d9c186
stonehenge51 <stonehenge515@gmail.com>
2021-11-02 11:02:20 -0700

    Test finished

--- q1/main.c
+++ q1/main.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 /*
 
 QUESTION 1:
@@ -9,10 +12,85 @@ you may have your function in the main.c file or create and link a separate file
 
 */
 
+void findRoots(double A, double B, double C, double* output)
+{
+	double root1 = 0.0, root1C = 0.0, root2 = 0.0, root2C = 0.0;
+
+	for(int i = 0; i < 6; i++)
+	{
+		output[i] = 0.0;
+	}
+
+	if(A != 0)
+	{
+		if((B*B - 4*A*C) < 0)
+		{
+			output[4] = 1.0;
+			double num1 = (-B) / (2 * A);
+			double num2 = -1 * (B*B - 4*A*C);
+
+			root1 = (-B) / (2 * A);
+			output[0] = root1;
+
+			root1C = sqrt(num2) / (2 * A);
+			output[1] = root1C;
+
+			output[2] = root1;
+
+			root2C = -1 * (sqrt(num2) / (2 * A));
+			output[3] = root2C;
+		}
+
+		else
+		{
+			double num1 = (-B) / (2 * A);
+			double num2 = sqrt(B*B - 4*A*C) / (2 * A);
+
+			root1 = num1 + num2;
+			output[0] = root1;
+
+			root2 = num1 - num2;
+			output[2] = root2;
+		}
+	}
+
+	else
+	{
+		output[5] = 1.0;
+		root1 = -C/B;
+		output[0] = root1;
+	}	
+}
 
 int main(int argc, char** argv)
 {
+	double A = 1.0;
+	double B = 2.0;
+	double C = 6.0;
+	double output[6];
+
+	findRoots(A, B, C, output);
+
+	if(output[5] == 1.0)
+	{
+		printf("The root of your linear equation is%f\n", output[0]);
+	}
+
+	else
+	{
+		if(output[4] == 1.0)
+		{
+			printf("The roots of your quadriatic function are complex\nRoot 1 is %0.3f + %0.3fi\nRoot 2 is %0.3f + %0.3fi\n",output[0], output[1], output[2], output[3]);
+		}
+
+		else
+		{
+			printf("The roots of your quadriatic function are real\nRoot 1 is %0.3f \nRoot 2 is %0.3f\n",output[0], output[2]);
+		}
+	}
+
 
 
   return 0;
 }
+
--- q2/main.c
+++ q2/main.c
@@ -1,13 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
 /*
 
 QUESTION 2:
 
-	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
+	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify 
+	the number of terms to calculate.
 */
 
+double factorioCalc(double number)
+{
+	if(number > 1)return number * factorioCalc(number-1);
+	else return 1.0;
+}
+
+double sinCalc(double x, int terms)
+{
+	double answer = 0.0;
+	int expNum = 3;
+	
+	for(int i = 0; i < terms; i++)
+	{
+		if(i == 0)
+		{
+			answer += x;
+		}
+
+		else
+		{
+			answer += pow(-1,i) * pow(x, 2*i+1) / factorioCalc(2*i+1);
+		}
+	}
+
+	return answer;
+}
+
+double cosCalc(double x, int terms)
+{
+	double answer = 0.0;
+	int expNum = 2;
+	
+	for(int i = 0; i < terms; i++)
+	{
+		if(i == 0)
+		{
+			answer += 1.0;
+		}
+
+		else
+		{
+			answer += pow(-1,i) * pow(x, 2*i) / factorioCalc(2*i);
+		}
+	}
+
+	return answer;
+}
 
 int main(int argc, char** argv)
 {
+	double x = 2.9*M_PI;
+	int terms = 50;
+
+	double ans = sinCalc(x, terms);
+	printf("The answer is %0.3f\n\n", ans);
+
+	ans = cosCalc(x, terms);
+	printf("The answer is %0.3f\n", ans);
 
 
   return 0;
