7200cc67a400aad30b7b755eaa97b8ed19eb4f9e
Dcamparmo <david.camparmo@email.kpu.ca>
2021-11-02 12:17:22 -0700

    Test Finished

--- q1/main.c
+++ q1/main.c
@@ -1,18 +1,46 @@
 /*
 
 QUESTION 1:
-write a function which will return the roots of a quadriatic including complex roots, it should take in the coefficients A,B and C  and should return both roots it should be able to cope with both real and complex roots and if given A = 0 should return the root of the linear equation.
+write a function which will return the roots of a quadriatic including complex roots, it should take in the coefficients A,B and C  and should 
+return both roots it should be able to cope with both real and complex roots and if given A = 0 should return the root of the linear equation.
 
 write a main function to use your function to find the roots of several polynomials, printing out the roots including if they are complex numbers.
 
 you may have your function in the main.c file or create and link a separate file as you wish
 
 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <complex.h>
+#include <math.h>
 
+
+void quadratic(double a, double b, double c)
+{
+double x1;
+double x2;
+double complex y1;
+double complex y2;
+if(4*a*c < b*b)
+ {
+	x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
+	x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
+	printf("x1 = %f\n",x1);
+	printf("x2 = %f\n",x2);
+ }
+ if(4*a*c > b*b)
+  {
+   y1=-b/(2*a)+sqrt(4*a*c-b*b)/(2*a)*I;
+   y2=-b/(2*a)-sqrt(4*a*c-b*b)/(2*a)*I;
+  	printf("x1=%f+%fi\n",creal(y1),cimag(y1));
+  	printf("x2=%f%fi\n",creal(y2),cimag(y2)); 
+  } 
+ 
+}
 
 int main(int argc, char** argv)
 {
-
-
+  quadratic(2,3,5);
   return 0;
 }
+
--- q2/main.c
+++ q2/main.c
@@ -2,13 +2,83 @@
 
 QUESTION 2:
 
-	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
+	write a function which will calculate the sin or 
+	\cos using the series expansion. The user of the function should be
+	 able to specify the the number of terms to calculate.
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void expandcos(double x,int i)
+{
+double y = 0;
+int s;
+int n;
+int m =-1;
+int fact[i];
+fact[0]=1;
+for(s=1;s<=i;s++)
+{
+	fact[s] = s*fact[s-1];
+	//printf("s = %i factorial = %i\n",s, fact[s]); 
+}
+for(s=0;s<20;s++)
+ {
+ 	if(s % 2 == 0)
+ 	{
+ 	m = m* -1;
+ 	n=fact[s];
+ 	y=m*pow(x,s)/n; // not switching from negative to positive 
+ 	printf("s = %i y = %f\n",s, y);
+    }
+ 	else
+ 	{
+
+ 	}
+ 	
+//printf("cos(%f) = %f\n",x,y);
+}
+printf("cos(%0.2f) = %0.2f\n",x,y);
+}
+
+void expandsin(double x, int i)
+{
+double y=0;
+int s;
+int n;
+int m = -1;
+double fact[i];
+fact[0]=1;
+for(s=1;s<=i;s++)
+{
+	fact[s] = s*fact[s-1];
+	//printf("s = %i factorial = %i\n",s, fact[s]); 
+}
+for(s=1;s<i;s++)
+ {
+ 	if(s % 2 == 0)
+ 	{
+ 	
+    }
+ 	else
+ 	{
+ 	m = m* -1;
+	n=fact[s];
+ 	y=y+m*pow(x,s)/n;
+ 	}
+ 	
+//printf("cos(%f) = %f\n",x,y);
+}
+printf("sin(%0.2f) = %0.2f\n",x,y);
+}
 
 
 int main(int argc, char** argv)
 {
-
+expandcos(3.14,20);
+expandsin(3.14,20);
 
   return 0;
 }
