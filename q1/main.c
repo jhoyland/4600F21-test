eb9774396978ccfb7052303f70b2c1f5c09aa4c1
James Hoyland <hoyland.jd@gmail.com>
2021-11-03 22:12:35 -0700

    Model solution

--- q1/main.c
+++ q1/main.c
@@ -9,10 +9,137 @@ you may have your function in the main.c file or create and link a separate file
 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/*
+  Solutions of quadratic:
+  A x^2 + B c + C =0
+
+  Coefficients sent as:
+  c[0] = C
+  c[1] = B
+  c[2] = A
+
+  returns x1 & x2 roots
+
+  x1[0] = real part
+  x1[1] = imag part
+*/
+void quadratic_roots(double * c, double * x1, double * x2)
+{
+  // Set imaginary parts to zero
+	x1[1] = 0;
+	x2[1] = 0;
+
+  // If squared coefficients are zero we have a linear equation. Set both real parts
+  // to the solution
+
+	if(c[2] == 0)
+	{
+		x1[0] = x2[0] = - c[0] / c[1];
+
+		return;
+	}
+
+  // Set the real parts to -B / 2A
+	x1[0] = x2[0] = - 0.5 * c[1] / c[2];
+
+  // Calculate the descriminant
+	double descriminant = c[1] * c[1] - 4.0 * c[0] * c[2];
+
+  // Calculate the sqrt( descriminant ) / 2A  using the absolute value of the
+	double term = 0.5 * sqrt(fabs(descriminant)) / c[2];
+
+  // If the descriminant is positive (real root) - add term to the real part
+  // if the descriminant is negative (complex root) - term is the imaginary part
+	if(descriminant >= 0)
+	{
+		x1[0] += term;
+		x2[0] -= term;
+	}
+	else
+	{
+		x1[1] = term;
+		x2[1] = -term;
+	}
+
+
+}
+
+// A little function for printing complex numbers
+
+void print_complex(double* x)
+{
+  printf("%0.5f ",x[0]);
+
+  if(x[1]<0)
+    printf("-");
+  else
+    printf("+");
+
+  printf(" i %0.5f",fabs(x[1]));
+}
+
+void print_quadratic(double* c)
+{
+  printf("%0.4f x^2 ",c[2]);
+  if(c[1]>=0) printf("+");
+  printf("%0.4f x ",c[1]);
+  if(c[0]>=0) printf("+");
+  printf("%0.4f",c[0]);
+}
 
 int main(int argc, char** argv)
 {
+    double coeff[3];
 
+    double x1[2];
+    double x2[2];
+
+    coeff[0] = -0.5;
+    coeff[1] = 5.3;
+    coeff[2] = 7;
+
+    print_quadratic(coeff);
+
+    quadratic_roots(coeff,x1,x2);
+
+    printf("\nx1 = ");
+    print_complex(x1);
+    printf("\nx2 = ");
+    print_complex(x2);
+
+    printf("\n\n");
+
+    coeff[0] = 2;
+    coeff[1] = 1;
+    coeff[2] = 7;
+
+    print_quadratic(coeff);
+
+    quadratic_roots(coeff,x1,x2);
+
+    printf("\nx1 = ");
+    print_complex(x1);
+    printf("\nx2 = ");
+    print_complex(x2);
+
+        printf("\n\n");
+
+    coeff[0] = 5;
+    coeff[1] = 4;
+    coeff[2] = 0;
+
+    print_quadratic(coeff);
+
+    quadratic_roots(coeff,x1,x2);
+
+    printf("\nx1 = ");
+    print_complex(x1);
+    printf("\nx2 = ");
+    print_complex(x2);
 
   return 0;
 }
--- q2/main.c
+++ q2/main.c
@@ -5,9 +5,89 @@ QUESTION 2:
 	write a function which will calculate the sin or cos using the series expansion. The user of the function should be able to specify the the number of terms to calculate.
 */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+#define SERIES_COS 0
+#define SERIES_SIN 1
+
+/*
+ The series function works for either sin or cos. In each case each successive term is calculated by multiplying the previous term by - x^2 / (m*(m-1))
+
+ x^2: because each term is power of 2 higher than the previous term.
+ m(m-1): the counter m increases by 2 for each term starting at 1 for sin or 0 for cos to give either odd or even numbers. m(m-1) multiplies the denominator by the new value of AND one less to give the correct factorial. This saves having to actually have a factorial function.
+ - : the minus sign makes sure the sign of each term alternates
+*/
+
+double trig_taylor(double x, int n, int trig_function)
+{
+	double r,t,m;
+
+  /* Set the first term to 1 or x */
+
+	if(trig_function == SERIES_COS)
+		t = 1;  // Cosine
+	else
+		t = x;  // Sin
+
+	r = t;
+	m = trig_function;  // m starts at 1 for sin and 0 for cos giving either even or odd numbers
+
+	for(int i=1;i<n;i++)
+	{
+		m += 2;
+		t *= - x * x / (m * (m-1));
+		r += t;
+
+	}
+
+	return r;
+}
+
+double series_sin(double x, int n)
+{
+  return trig_taylor(x,n,SERIES_SIN);
+}
+
+double series_cos(double x, int n)
+{
+  return trig_taylor(x,n,SERIES_COS);
+}
 
 int main(int argc, char** argv)
 {
+
+  double x = 0.5 * M_PI;
+
+  int n = 14;
+
+  char* p;
+
+  if(argc>1)
+  {
+    x = strtod(argv[1],&p);
+    if(argc>2)
+      n = strtol(argv[2],&p,10);
+  }
+
+  if(n<1) n = 1;
+
+  double sm = sin(x);
+  double cm = cos(x);
+
+  double st,ct;
+
+  printf("\nmath.h sin of %f = %f",x,sm);
+  printf("\nmath.h cos of %f = %f",x,cm);
+
+  printf("\n Series solutions: \nTERMS\tSIN\t\tCOS");
+  for(int j = 1; j<=n; j++)
+  {
+    st = series_sin(x,j);
+    ct = series_cos(x,j);
+    printf("\n%d\t%9.7f\t%9.7f",j,st,ct);
+  }
 
 
   return 0;
