fc39945dcb85df0c35861d0c5ff4dabb963571c8
James Hoyland <hoyland.jd@gmail.com>
2021-11-08 22:54:42 -0800

    Marked

--- q1/main.c
+++ q1/main.c
@@ -1,7 +1,7 @@
 /*
 
 QUESTION 1:
-write a function which will return the roots of a quadriatic including complex roots, it should take in the coefficients A,B and C  and should 
+write a function which will return the roots of a quadriatic including complex roots, it should take in the coefficients A,B and C  and should
 return both roots it should be able to cope with both real and complex roots and if given A = 0 should return the root of the linear equation.
 
 write a main function to use your function to find the roots of several polynomials, printing out the roots including if they are complex numbers.
@@ -15,12 +15,25 @@ you may have your function in the main.c file or create and link a separate file
 #include <math.h>
 
 
+/*
+JAMES: The function was supposed to return the roots. It was also supposed to cope with the
+the special case of a linear root if a = 0.
+*/
+
 void quadratic(double a, double b, double c)
 {
 double x1;
 double x2;
 double complex y1;
 double complex y2;
+
+/*JAMES: What happens if 4*a*c == b*b? */
+
+/*JAMES: So this will work fine, but there is an efficiency problem here:
+You calculate b*b-4*a*c multiple times (in your tests and when assigning the roots.) It would
+be much more efficient if you calculated this once and put the value in a local variable.
+*/
+
 if(4*a*c < b*b)
  {
 	x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
@@ -33,9 +46,9 @@ if(4*a*c < b*b)
    y1=-b/(2*a)+sqrt(4*a*c-b*b)/(2*a)*I;
    y2=-b/(2*a)-sqrt(4*a*c-b*b)/(2*a)*I;
   	printf("x1=%f+%fi\n",creal(y1),cimag(y1));
-  	printf("x2=%f%fi\n",creal(y2),cimag(y2)); 
-  } 
- 
+  	printf("x2=%f%fi\n",creal(y2),cimag(y2));
+  }
+
 }
 
 int main(int argc, char** argv)
@@ -43,4 +56,3 @@ int main(int argc, char** argv)
   quadratic(2,3,5);
   return 0;
 }
-
--- q2/main.c
+++ q2/main.c
@@ -2,7 +2,7 @@
 
 QUESTION 2:
 
-	write a function which will calculate the sin or 
+	write a function which will calculate the sin or
 	\cos using the series expansion. The user of the function should be
 	 able to specify the the number of terms to calculate.
 */
@@ -10,6 +10,11 @@ QUESTION 2:
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+
+/*JAMES: Again the function should generally return the value of sin or cosine.
+printf functions inside a function like this are to be avoided: it means
+the function can only really be used in a program designed for running in a
+terminal window.*/
 
 void expandcos(double x,int i)
 {
@@ -19,25 +24,28 @@ int n;
 int m =-1;
 int fact[i];
 fact[0]=1;
-for(s=1;s<=i;s++)
+for(s=1;s<=i;s++)  /*JAMES: This is quite a nice way to do the factorials since
+in this application you do actually want all of them*/
 {
 	fact[s] = s*fact[s-1];
-	//printf("s = %i factorial = %i\n",s, fact[s]); 
+	//printf("s = %i factorial = %i\n",s, fact[s]);
 }
+/*JAMES: You've hardcoded the number of terms here meaning that if i is not 20
+the function will not work as intended */
 for(s=0;s<20;s++)
  {
- 	if(s % 2 == 0)
+ 	if(s % 2 == 0) // JAMES: Since you are skipping every other, you could put s+=2 instead of s++ in the for statement
  	{
  	m = m* -1;
  	n=fact[s];
- 	y=m*pow(x,s)/n; // not switching from negative to positive 
+ 	y=m*pow(x,s)/n; // not switching from negative to positive
  	printf("s = %i y = %f\n",s, y);
     }
  	else
  	{
-
+		/* JAMES:you don't need the else section if it's empty */
  	}
- 	
+
 //printf("cos(%f) = %f\n",x,y);
 }
 printf("cos(%0.2f) = %0.2f\n",x,y);
@@ -54,13 +62,13 @@ fact[0]=1;
 for(s=1;s<=i;s++)
 {
 	fact[s] = s*fact[s-1];
-	//printf("s = %i factorial = %i\n",s, fact[s]); 
+	//printf("s = %i factorial = %i\n",s, fact[s]);
 }
 for(s=1;s<i;s++)
  {
  	if(s % 2 == 0)
  	{
- 	
+
     }
  	else
  	{
@@ -68,7 +76,7 @@ for(s=1;s<i;s++)
 	n=fact[s];
  	y=y+m*pow(x,s)/n;
  	}
- 	
+
 //printf("cos(%f) = %f\n",x,y);
 }
 printf("sin(%0.2f) = %0.2f\n",x,y);
